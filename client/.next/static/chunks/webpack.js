4bfdc754cc4b9e6003e228089ab044daa543ad02
Angela Palaszewski <delarosa.ang@gmail.com>
2023-06-19 11:03:59 -0600

    switched from clubs to backend

--- client/.next/static/chunks/webpack.js
+++ client/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "99b1f1fcf0f02ced"; }
+/******/ 		__webpack_require__.h = function() { return "3e1ac72b7d255000"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- client/.next/static/webpack/webpack.f0458b23b83ee00e.hot-update.js
+++ client/.next/static/webpack/webpack.f0458b23b83ee00e.hot-update.js
@@ -35,7 +35,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ 
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "99b1f1fcf0f02ced"; }
+/******/ 	__webpack_require__.h = function() { return "6a2222e6927662ed"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/global */
